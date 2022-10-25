b5dd094108ae5491319308d235b2542c7c7f49b6
Kauserul <kauserulislam0055@gmail.com>
2022-10-25 23:08:26 +0600

    api running

--- index.js
+++ index.js
@@ -6,9 +6,26 @@ const port = process.env.PORT || 5000;
 app.use(cors())
 
 const course = require('./data/course.json')
+const category = require('./data/category.json')
 
 app.get('/', (req, res) =>{
     res.send('API Running soon...')
+})
+
+app.get('/courses/:id', (req, res) =>{
+    const id = req.params.id;
+    const selectedCourse = course.find( c => c.id == id)
+    res.send(selectedCourse)
+})
+
+app.get('/all-category' , (req, res) =>{
+    res.send(category)
+})
+
+app.get('/category/:id', (req, res)=>{
+    const id = req.params.id;
+    const categoryItem = course.filter( c => c.id == id)
+    res.send(categoryItem)
 })
 
 app.get('/courses', (req, res) =>{
